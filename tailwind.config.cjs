3eaa1738f272b9b90286a239b0fdfb7423587030
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2022-10-18 21:33:37 +0530

    init

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        josefin: ['Josefin Sans', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
